dba72e486a809c8c593ec2281b5c4d6f589e7697
Albin Severinson <albin@severinson.org>
2020-07-22 22:58:30 +0200

    development scratchpad

--- src/Scratchpad.jl
+++ src/Scratchpad.jl
@@ -17,7 +17,7 @@ function plot_bounds(k=600, M=40, δ=1e-6)
     ps = pdf.(Ω, ds)
     ps ./= sum(ps)
 
-    γs = range(1, 1.02, length=20)
+    γs = range(1, 1.04, length=20)
     for q in [2, 3, 4, 5, 6, 7, 8]
         @views fs = upperbound_ltfailure.(γs, k=k, q=q, ds=ds, ps=ps)
         plt.semilogy(k.*(γs.-1), fs, ".-", label="GF($q)")
@@ -26,8 +26,32 @@ function plot_bounds(k=600, M=40, δ=1e-6)
     plt.semilogy(k.*(γs.-1), fs, "k-", label="LB")
 
     plt.grid()
-    # plt.xlim(γs[1], γs[end])
-    plt.ylim(1e-6, 1)
+    plt.xlim(0, 15)
+    plt.ylim(1e-4, 1)
+    plt.xlabel("Reception overhead")
+    plt.ylabel("Decoding failure probability")
+    plt.legend()
+    plt.tight_layout()
+    return
+end
+
+function plot_bounds_uniform(k=50)
+
+    ds = 1:k
+    ps = [1/k for _ in 1:k]
+
+    γs = range(1, 1.5, length=20)
+    for q in [2, 4, 6, 8]
+        println("q=$q")
+        @views fs = upperbound_ltfailure.(γs, k=k, q=q, ds=ds, ps=ps)
+        plt.semilogy(k.*(γs.-1), fs, ".-", label="GF($q)")
+    end
+    fs = lowerbound_ltfailure.(γs, k=k, ds=ds, ps=ps)
+    plt.semilogy(k.*(γs.-1), fs, "k-", label="LB")
+
+    plt.grid()
+    # plt.xlim(0, 12)
+    # plt.ylim(1e-4, 1)
     plt.xlabel("Reception overhead")
     plt.ylabel("Decoding failure probability")
     plt.legend()
